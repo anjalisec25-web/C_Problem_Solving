0b22352774ba5c751fc03502165f09a20e33dbd8
ANJALI S <anjalis.ec25@bitsathy.ac.in>
2026-09-07 21:55:57 +0530

    Added Frequecy of string Program

--- String/Frequency_of_string.c
+++ String/Frequency_of_string.c
@@ -1,16 +1,17 @@
 #include <stdio.h>
 int main(){
     char str[100];
-    int freq[256]={0},i;
-    printf("Enter a String:");
+    int i,freq[256]={0};
+    printf("Enter a string:");
     scanf("%s",str);
     for(i=0;str[i]!='\0';i++){
         freq[(unsigned char)str[i]]++;
     }
-    printf("Frequency of Each Character,\n");
-    for(i=0;i<256;i++){
-        if(str[i]!='\n' && freq[i]!=0){
-            printf("%c = %d\n",i,freq[i]);
+    printf("Frequency of each character,\n");
+    for(i=0;str[i]!='\0';i++){
+        if(freq[(unsigned char)str[i]]!=0){
+            printf("%c = %d\n",str[i],freq[(unsigned char)str[i]]);
+            freq[(unsigned char)str[i]]=0;
         }
     }
     return 0;
