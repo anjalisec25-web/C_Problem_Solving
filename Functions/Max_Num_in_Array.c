afba203ccdef843a6293b8bd334528e4a2572028
ANJALI S <anjalis.ec25@bitsathy.ac.in>
2026-09-21 09:34:36 +0530

    Added Max num in array Program

---
+++ Functions/Max_Num_in_Array.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+int maxarr(int a[],int n){
+    int max=a[0],i;
+    for(i=0;i<n;i++){
+        if(a[i]>max){
+            max=a[i];
+        }
+    }
+    return max;
+}
+int main(){
+    int n,i;
+    printf("Enter Number of elements:");
+    scanf("%d",&n);
+    int a[n];
+    for(i=0;i<n;i++){
+        printf("Emter Numbers:");
+        scanf("%d",&a[i]);
+    }
+    printf("Maximum Number in array is,%d",maxarr(a,n));
+    return 0;
+}
